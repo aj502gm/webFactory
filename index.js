0bd8b7ae665a2e912d636a1fe4c5c5cacb6e1ebf
aj502gm <androsogt@gmail.com>
2021-09-21 15:15:21 -0600

    Uploading html, css and js file is now in progress

--- index.js
+++ index.js
@@ -59,3 +59,16 @@ function updateIframe(origin){
     var myFrame = $("#outputTxt").contents().find('body');
     myFrame.html(origin);
 }
+
+function displayDialog(e){
+    let input = document.createElement('input');
+    input.type = 'file';
+    input.accept = (e.target.id === 'htmlSelector')? '.html': (e.target.id === 'cssSelector')? '.css' : '.js';
+    input.onchange = _ => {
+    // you can use this method to get file and perform respective operations
+        let files =   Array.from(input.files);
+        console.log(files);
+    };
+    input.click();
+  
+}
